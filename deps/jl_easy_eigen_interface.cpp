7f4228ce4e9840ceee58a95f157740b7be4f2493
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2024-08-12 17:13:36 +0900

    Wrap MatrixNd MatrixNf where N = 2,3,4 and X

--- deps/jl_easy_eigen_interface.cpp
+++ deps/jl_easy_eigen_interface.cpp
@@ -5,7 +5,7 @@
 
 namespace jleigen
 {
-  struct WrapEigenMatrix
+  struct WrapEigenMatrixX
   {
     template<typename TypeWrapperT>
     void operator()(TypeWrapperT&& wrapped)
@@ -19,10 +19,10 @@ namespace jleigen
       wrapped.module().set_override_module(jl_base_module);
       wrapped.method("resize!", [](WrappedT& m, int_t i, int_t j) {
         m.resize(i, j);
-      }); 
+      });
       wrapped.module().method("getindex", [](const WrappedT& m, int_t i, int_t j) { return m(i-1,j-1); });
       wrapped.module().method("setindex!", [](WrappedT& m, ScalarT value, int_t i, int_t j) { m(i-1,j-1) = value; });
-      
+
       wrapped.module().method("getindex", [](const WrappedT& m, int_t i) { return m(i-1); });
       wrapped.module().method("setindex!", [](WrappedT& m, ScalarT value, int_t i) { m(i-1) = value; });
 
@@ -30,7 +30,29 @@ namespace jleigen
     }
   };
 
-  struct WrapEigenVector
+  struct WrapEigenMatrixStaticSized
+  {
+    template<typename TypeWrapperT>
+    void operator()(TypeWrapperT&& wrapped)
+    {
+      using WrappedT = typename TypeWrapperT::type;
+      using ScalarT = typename WrappedT::Scalar;
+      wrapped.method("cols", &WrappedT::cols);
+      wrapped.method("rows", &WrappedT::rows);
+
+      wrapped.module().set_override_module(jl_base_module);
+
+      wrapped.module().method("getindex", [](const WrappedT& m, int_t i, int_t j) { return m(i-1,j-1); });
+      wrapped.module().method("setindex!", [](WrappedT& m, ScalarT value, int_t i, int_t j) { m(i-1,j-1) = value; });
+
+      wrapped.module().method("getindex", [](const WrappedT& m, int_t i) { return m(i-1); });
+      wrapped.module().method("setindex!", [](WrappedT& m, ScalarT value, int_t i) { m(i-1) = value; });
+
+      wrapped.module().unset_override_module();
+    }
+  };
+
+  struct WrapEigenVectorX
   {
     template<typename TypeWrapperT>
     void operator()(TypeWrapperT&& wrapped)
@@ -47,26 +69,86 @@ namespace jleigen
       wrapped.module().unset_override_module();
     }
   };
+
+  struct WrapEigenVectorStaticSized
+  {
+    template<typename TypeWrapperT>
+    void operator()(TypeWrapperT&& wrapped)
+    {
+      using WrappedT = typename TypeWrapperT::type;
+      using ScalarT = typename WrappedT::Scalar;
+      wrapped.method("cols", &WrappedT::cols);
+      wrapped.method("rows", &WrappedT::rows);
+
+      wrapped.module().set_override_module(jl_base_module);
+      wrapped.module().method("getindex", [](const WrappedT& v, int_t i) { return v(i-1); });
+      wrapped.module().method("setindex!", [](WrappedT& v, ScalarT value, int_t i) { v(i-1) = value; });
+      wrapped.module().unset_override_module();
+    }
+  };
 }
 
 namespace jlcxx
 {
 
+// MatrixXd
 template<typename T>
 struct BuildParameterList<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>>
 {
   typedef ParameterList<T> type;
 };
 
+// VectorXd
 template<typename T>
 struct BuildParameterList<Eigen::Matrix<T, Eigen::Dynamic, 1>>
 {
   typedef ParameterList<T> type;
 };
 
+// Matrix
+template<typename T>
+struct BuildParameterList<Eigen::Matrix<T, 2, 2>>
+{
+  typedef ParameterList<T> type;
+};
+
+template<typename T>
+struct BuildParameterList<Eigen::Matrix<T, 3, 3>>
+{
+  typedef ParameterList<T> type;
+};
+
+template<typename T>
+struct BuildParameterList<Eigen::Matrix<T, 4, 4>>
+{
+  typedef ParameterList<T> type;
+};
+
+// Vector
+template<typename T>
+struct BuildParameterList<Eigen::Matrix<T, 2, 1>>
+{
+  typedef ParameterList<T> type;
+};
+
+template<typename T>
+struct BuildParameterList<Eigen::Matrix<T, 3, 1>>
+{
+  typedef ParameterList<T> type;
+};
+
+template<typename T>
+struct BuildParameterList<Eigen::Matrix<T, 4, 1>>
+{
+  typedef ParameterList<T> type;
+};
 }
 
 Eigen::MatrixXd example1(Eigen::MatrixXd x){
+  return 3 * x;
+}
+
+Eigen::Matrix2d example2(Eigen::Matrix2d x){
   return 3 * x;
 }
 
@@ -75,10 +157,30 @@ JLCXX_MODULE easy_eigen_interface(jlcxx::Module& mod)
   using jlcxx::Parametric;
   using jlcxx::TypeVar;
 
-  mod.add_type<Parametric<TypeVar<1>>>("EigenMatrix", jlcxx::julia_type("AbstractEigenMatrix"))
-    .apply<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>, Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>>(jleigen::WrapEigenMatrix());
-  mod.add_type<Parametric<TypeVar<1>>>("EigenVector", jlcxx::julia_type("AbstractEigenVector"))
-    .apply<Eigen::Matrix<double, Eigen::Dynamic, 1>, Eigen::Matrix<float, Eigen::Dynamic, 1>>(jleigen::WrapEigenVector());
+  // Matrix
+  mod.add_type<Parametric<TypeVar<1>>>("EigenMatrixX", jlcxx::julia_type("AbstractEigenMatrix"))
+    .apply<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>, Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>>(jleigen::WrapEigenMatrixX());
+
+  // Vectoor
+  mod.add_type<Parametric<TypeVar<1>>>("EigenVectorX", jlcxx::julia_type("AbstractEigenVector"))
+    .apply<Eigen::Matrix<double, Eigen::Dynamic, 1>, Eigen::Matrix<float, Eigen::Dynamic, 1>>(jleigen::WrapEigenVectorX());
+
+  #define ADD_EIGEN_MATRIX_TYPE(N) \
+    mod.add_type<Parametric<TypeVar<1>>>("EigenMatrix" #N, jlcxx::julia_type("AbstractEigenMatrix")) \
+      .apply<Eigen::Matrix<double, N, N>, Eigen::Matrix<float, N, N>>(jleigen::WrapEigenMatrixStaticSized());
+
+  #define ADD_EIGEN_VECTOR_TYPE(N) \
+    mod.add_type<Parametric<TypeVar<1>>>("EigenVector" #N, jlcxx::julia_type("AbstractEigenVector")) \
+      .apply<Eigen::Matrix<double, N, 1>, Eigen::Matrix<float, N, 1>>(jleigen::WrapEigenVectorStaticSized());
+
+  ADD_EIGEN_MATRIX_TYPE(2)
+  ADD_EIGEN_MATRIX_TYPE(3)
+  ADD_EIGEN_MATRIX_TYPE(4)
+
+  ADD_EIGEN_VECTOR_TYPE(2)
+  ADD_EIGEN_VECTOR_TYPE(3)
+  ADD_EIGEN_VECTOR_TYPE(4)
 
   mod.method("example1", &example1);
+  mod.method("example2", &example2);
 }
